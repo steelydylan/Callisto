b8f66bc36be5d6bc02ad8adff979675c8d681636
steelydylan <ess_president@me.com>
2014-11-03 00:41:57 +0900

    offcanvas左右実装

--- callisto.js
+++ callisto.js
@@ -48,20 +48,34 @@ $(function(){
 		$target.css('order',order_o).animate({opacity:0},200).animate({opacity:1},200);
 	});
 	$("body").addClass("cs-offcanvasBody");
-	$(".cs-right-open-btn").click(function(){
+	$(".cs-right-open-btn,.cs-left-open-btn").click(function(){
 		var $target = $($(this).attr("href"));
-		$(".cs-offcanvasBody").addClass("active");
+		var $that = $(this);
+		if($that.hasClass("cs-right-open-btn")){
+			$(".cs-offcanvasBody").addClass("rightActive");
+		}else if($that.hasClass("cs-left-open-btn")){
+			$(".cs-offcanvasBody").addClass("leftActive");
+		}
 		$target.addClass("active");
+		var $sidebar = $target.find(".cs-offcanvas-sidebar");
+		if($that.hasClass("cs-right-open-btn")){
+			$sidebar.addClass("rightActive");
+		}else{
+			$sidebar.addClass("leftActive");
+		}
 		setTimeout(function(){
-			$target.find(".cs-offcanvas-sidebar").addClass("active");
+			$sidebar.addClass("active");
 		},0);
 	});
 	$(".cs-offcanvas").click(function(e){
 		if(!$(e.target).hasClass("cs-offcanvas-sidebar") && $(e.target).parent("cs-offcanvas-sidebar").length == 0){
+				$(".cs-offcanvasBody").removeClass("rightActive");
+				$(".cs-offcanvasBody").removeClass("leftActive");
 				$(".cs-offcanvas-sidebar").removeClass("active");
-				$(".cs-offcanvasBody").removeClass("active");
 			setTimeout(function(){
 				$(".cs-offcanvas").removeClass("active");
+				$(".cs-offcanvas-sidebar").removeClass('rightActive');
+				$(".cs-offcanvas-sidebar").removeClass('leftActive');
 			},300);
 		}
 	});
