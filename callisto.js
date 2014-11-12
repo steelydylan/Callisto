7acf5ca0652e6a8f8160a4f00942e20a18310f12
steelydylan <ess_president@me.com>
2014-11-12 22:55:26 +0900

    スライダーを追加

--- callisto.js
+++ callisto.js
@@ -8,6 +8,28 @@ $.fn.extend({
 		$(this).removeClass("active");
 		$(this).find(".cs-modal_content").removeClass("active");
 	},
+	slider:function(json){
+		var json = json || {};
+		json.items = json.items || 3;
+		json.time = json.time || 60;
+		var $that = $(this);
+		$that.wrap("<div class='cs-slider-wrapper'></div>");
+		$parent = $that.parent(".cs-slider-wrapper");
+		var width = $parent.width();
+		var itemWidth = parseInt(width / json.items);
+		var $children = $that.children("li");
+		$children.width(itemWidth);
+		$that.width($children.length * itemWidth);
+		var $btnWrapper = $("<div class='cs-slider-btn-wrapper'>");
+		var cnt = Math.ceil($children.length / json.items);
+		var $btn = $("<div class='cs-slider-btn'></div>");
+		for(var i = 0; i < cnt; i++){
+			$btnWrapper.append($btn.clone());
+		}
+		$parent.append($btnWrapper);
+		$that.attr("data-items",json.items);
+		$that.attr("data-time",json.time);
+	}
 });
 $(function(){
 	$('[data-toggle="showModal"]').click(function(){
@@ -92,6 +114,27 @@ $(function(){
 				})
 			}
 		}
+	});
+	$(document).on("click",".cs-slider-btn",function(){
+		$(".cs-slider-btn").removeClass('active');
+		$(this).addClass('active');
+		var index = $(".cs-slider-btn").index(this);
+		var $slider = $(this).parent().parent(".cs-slider-wrapper").children(".cs-slider");
+		var itemWidth = $slider.children("li").width();
+		var left = $slider.css("left") ==  "auto" ? 0 : parseInt($slider.css("left"));
+		var offset = -left - (parseInt($slider.data("items")) * itemWidth * index);
+		var time = $slider.data("time");
+		var cnt = 0;
+		var add = offset / parseInt(time);
+		var interval = setInterval(function(){
+			if(cnt <= time){
+				x = left + offset*(cnt/time);
+			}else{
+				clearInterval(interval);
+			}
+			cnt++;
+			$slider.css("left",x+"px");
+		});
 	});
 });
 
